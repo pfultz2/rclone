55ae38271f7dd95664dbc9fd2a476c40be5dc551
Paul <pfultz2@yahoo.com>
2018-07-23 00:06:48 -0500

    Recursive clone

--- rclone/cli.py
+++ rclone/cli.py
@@ -1,16 +1,78 @@
-import subprocess, click
+import subprocess, click, tarfile, urllib, os, exceptions, ConfigParser, StringIO, tempfile
 
 from rclone import __version__
+
+def extract_ar(archive, dst, **kwargs):
+    # TODO: Delete directory
+    d = tempfile.mkdtemp()
+    tarfile.open(archive, **kwargs).extractall(d)
+    f = os.path.join(d, os.listdir(d)[0])
+    os.rename(f, dst)
+
+class RCloneURLOpener(urllib.FancyURLopener):
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        if errcode >= 400:
+            raise exceptions.RuntimeError("Download failed with error {0} for: {1}".format(errcode, url))
+        return urllib.FancyURLopener.http_error_default(self, url, fp, errcode, errmsg, headers)
+
+def download(url, filename):
+    RCloneURLOpener().retrieve(url, filename=filename)
+    return filename
+        
+class Submodule:
+    def __init__(self, repo, name, url, path, commit):
+        self.repo = repo
+        self.name = name
+        self.url = url
+        self.path = path
+        self.commit = commit
+
+    def fetch(self):
+        if 'github.com' in self.url:
+            start = self.url.find('github.com')+len('github.com/')
+            gh = self.url[start:].replace('.git', '')
+            f = os.path.join(self.repo, self.name+'.tar.gz')
+            download('https://github.com/{}/archive/{}.tar.gz'.format(gh, self.commit), f)
+            extract_ar(f, self.path, mode='r:gz')
+            os.remove(f)
+        else:
+            subprocess.check_call(['git', 'clone', self.url, self.path])
+            subprocess.check_call(['git', 'checkout', self.commit], cwd=self.path)
+
+# Strip the config file of whitespaces
+def stripfile(f):
+    return StringIO.StringIO('\n'.join((line.strip() for line in open(f).readlines())))
+
+def get_submodules(repo):
+    commits = {}
+    for line in subprocess.check_output(['git', 'submodule'], cwd=repo).splitlines():
+        words = line.split()
+        commit = words[0].strip('-')
+        path = words[1]
+        commits[path] = commit
+    if len(commits) > 0:
+        config = ConfigParser.ConfigParser(allow_no_value=True)
+        config.readfp(stripfile(os.path.join(repo, '.gitmodules')))
+        for section in config.sections():
+            if section.startswith('submodule "'):
+                name = section[11:-1]
+                url = config.get(section, 'url')
+                path = config.get(section, 'path')
+                fullpath = os.path.join(repo, path)
+                commit = commits[path]
+                yield Submodule(repo, name, url, fullpath, commit)
+
 
 @click.command(context_settings={'help_option_names': ['-h', '--help']})
 @click.version_option(version=__version__, prog_name='rclone')
 @click.option('-b', '--branch')
 @click.argument('url', required=True)
-@click.argument('path', required=False)
+@click.argument('path', required=True)
 def cli(branch, url, path):
     clone_cmd = ['git', 'clone', '--depth', '1']
     if branch: clone_cmd.extend(['-b', branch])
-    if url: clone_cmd.extend([url])
-    if path: clone_cmd.extend([path])
-    subprocess.check_call(clone_cmd)
+    subprocess.check_call(clone_cmd+[url, path])
+    for m in get_submodules(path):
+        m.fetch()
+
 
--- test/test_rclone.py
+++ test/test_rclone.py
@@ -4,11 +4,17 @@ def test_version():
     subprocess.check_call(['rclone', '--version'])
 
 def test_simple_clone(tmpdir):
-    subprocess.check_call(['rclone', 'https://github.com/pfultz2/rclone'], cwd=tmpdir.strpath)
+    repo = os.path.join(tmpdir.strpath, 'repo')
+    subprocess.check_call(['rclone', 'https://github.com/pfultz2/rclone', repo])
 
 def test_simple_clone_branch(tmpdir):
-    subprocess.check_call(['rclone', '-b', 'master', 'https://github.com/pfultz2/rclone'], cwd=tmpdir.strpath)
+    repo = os.path.join(tmpdir.strpath, 'repo')
+    subprocess.check_call(['rclone', '-b', 'master', 'https://github.com/pfultz2/rclone', repo])
 
-def test_simple_clone_path(tmpdir):
-    subprocess.check_call(['rclone', '-b', 'master', 'https://github.com/pfultz2/rclone', os.path.join(tmpdir.strpath, 'repo')])
+def test_recursive_clone(tmpdir):
+    repo = os.path.join(tmpdir.strpath, 'repo')
+    subprocess.check_call(['rclone', 'https://github.com/RadeonOpenCompute/hcc.git', repo])
+    print os.listdir(os.path.join(repo, 'clang'))
+    assert os.path.exists(os.path.join(repo, 'clang', 'CMakeLists.txt'))
+
 
