374efedfb9075738773fc0065e34d394b1411625
Paul <pfultz2@yahoo.com>
2018-07-22 19:45:37 -0500

    Basic cloning test

--- rclone/cli.py
+++ rclone/cli.py
@@ -2,9 +2,15 @@ import subprocess, click
 
 from rclone import __version__
 
-@click.group(context_settings={'help_option_names': ['-h', '--help']})
+@click.command(context_settings={'help_option_names': ['-h', '--help']})
 @click.version_option(version=__version__, prog_name='rclone')
-@click.argument('url')
-def cli(url):
-    subprocess.check_call('git clone --depth 1 {url}'.format(url=url))
+@click.option('-b', '--branch')
+@click.argument('url', required=True)
+@click.argument('path', required=False)
+def cli(branch, url, path):
+    clone_cmd = ['git', 'clone', '--depth', '1']
+    if branch: clone_cmd.extend(['-b', branch])
+    if url: clone_cmd.extend([url])
+    if path: clone_cmd.extend([path])
+    subprocess.check_call(clone_cmd)
 
--- test/test_rclone.py
+++ test/test_rclone.py
@@ -1,5 +1,14 @@
-import subprocess
+import subprocess, os
 
 def test_version():
     subprocess.check_call(['rclone', '--version'])
+
+def test_simple_clone(tmpdir):
+    subprocess.check_call(['rclone', 'https://github.com/pfultz2/rclone'], cwd=tmpdir.strpath)
+
+def test_simple_clone_branch(tmpdir):
+    subprocess.check_call(['rclone', '-b', 'master', 'https://github.com/pfultz2/rclone'], cwd=tmpdir.strpath)
+
+def test_simple_clone_path(tmpdir):
+    subprocess.check_call(['rclone', '-b', 'master', 'https://github.com/pfultz2/rclone', os.path.join(tmpdir.strpath, 'repo')])
 
